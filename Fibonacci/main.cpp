d6a83beb54f3e8b6485997135aa2a0c49d80ff97
Evgeny Zhdanovskiy <zhdanovskiy@playform.com>
2016-08-22 11:57:40 +0300

    Fibonacci : the last digit of a large number

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -3,19 +3,17 @@
 
 class Fibonacci final {
 public:
-    static int get(int n) {
-        assert(n >= 0);
+    static int get_last_digit(int n) {
+        assert(n >= 1);
         // put your code here
-        assert(n <= 40);
-        int n1 = n;
-        int v[41];
-        v[0] = 0;
-        v[1] = 1;
-        int i = 2;
-        for (; i <= n1; ++i) {
-            v[i] = v[i-1] + v[i-2];
+        int f1 = 0;
+        int f2 = 1;
+        for (int i = 2; i <= n; ++i) {
+            int f = (f2 + f1) % 10;
+            f1 = f2;
+            f2 = f;
         }
-        n = v[i-1];
+        n = f2;
         return n;
     }
 };
@@ -23,6 +21,6 @@ public:
 int main(void) {
     int n;
     std::cin >> n;
-    std::cout << Fibonacci::get(n) << std::endl;
+    std::cout << Fibonacci::get_last_digit(n) << std::endl;
     return 0;
 }
